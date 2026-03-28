dc9c15d945ab2b5cad5a4b494b2b6dd547055d2b
sidV214 <vermasid2107@gmail.com>
2026-03-29 00:16:02 +0530

    Hardcode production cookie security parameters

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -14,8 +14,8 @@ export const googleAuth = async (req, res) => {
         let token = await generateToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 

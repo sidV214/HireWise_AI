5c8f1f26b08a6db620306de916497a51989b89d0
sidV214 <vermasid2107@gmail.com>
2026-03-31 16:59:56 +0530

    Update session cookie and JWT to last indefinitely

--- server/config/token.js
+++ server/config/token.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 const generateToken = async (userId) => {
     try {
-        const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" })
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY)
         return token
     } catch (error) {
         console.log("Token related error: ", error)
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const googleAuth = async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: "none",
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 100 * 365 * 24 * 60 * 60 * 1000 // Lasts until explicitly logged out (100 years)
         })
 
         return res.status(200).json(user)
